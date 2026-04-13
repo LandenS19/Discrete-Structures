39a72acb07286fa390ef288fd1ea714b334b1920
LandenS19 <landen.summers19@gmail.com>
2026-04-13 11:38:53 -0500

    Updates on testing

--- Test.java
+++ Test.java
@@ -1,12 +1,19 @@
 public class Test {
     
     public static void main(String[] args) {
-        String x = " ";
-        char[] chars = {'s', 'i', 'c', 'k'};
-        String newX = "";
-        for(char c : chars){
-            newX += c;
-        }
-        System.out.println(newX);
+       System.out.println(A(17));
+    }
+
+    static int A(int n){
+        if(n==0) return 1;
+        return C(n-1);
+    }
+    static int B(int n){
+        if(n==0) return 0;
+        return A(n-1) + C(n-1);
+    }
+    static int C(int n){
+        if(n==0) return 0;
+        return B(n-1) + C(n-1);
     }
 }
